74711565ac3820b59bb01f5cdf04398dd685a47d
Pseudomoridin <73117281+Pseudomoridin@users.noreply.github.com>
2021-09-08 11:41:58 -0500

    Initial Setup and commit

---
+++ Lab2a_Act3.py
@@ -0,0 +1,10 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+# Names:        Elayne Elphingstone
+#               NAME OF TEAM MEMBER 2
+#               NAME OF TEAM MEMBER 3
+#               NAME OF TEAM MEMBER 4
+# Section:      472/572
+# Assignment:   Lab 2A Activity 3
+# Date:         September 8, 2021
