32157d4da6fa73f8dd19ce422c65d24b92c1f31c
Pseudomoridin <73117281+Pseudomoridin@users.noreply.github.com>
2021-09-08 12:49:27 -0500

    initial version of Activity 3

--- Lab2a_Act3.py
+++ Lab2a_Act3.py
@@ -8,3 +8,32 @@
 # Section:      472/572
 # Assignment:   Lab 2A Activity 3
 # Date:         September 8, 2021
+
+#imports
+import math as m
+
+"""
+Method declaration
+"""
+def get_distance_linear(time):
+    return SLOPE * time + YINT
+
+def get_distance_orbit(time):
+    return (SLOPE * time + YINT) % ORBIT_CIRCUMFERENCE
+
+"""
+Variable declaration
+"""
+ORBIT_RADIUS = 6745
+ORBIT_CIRCUMFERENCE = 2 * m.pi * ORBIT_RADIUS
+SLOPE = 21000.0 / 45.0
+YINT = -(SLOPE * 10) + 2025
+
+time = 0
+final_dist = 0
+
+"""
+Main
+"""
+print(get_distance_linear(time))
+print(get_distance_orbit(time))
